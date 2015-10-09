1a991ac0e79c3332a064249dd78860fd787889d7
Trevelyn412 <weaknetlabs@gmail.com>
2015-10-09 17:26:21 +0000

    HUGE updates! Fixed bugs! New icons and HTML logs!

--- lib/GMAPS.pm
+++ lib/GMAPS.pm
@@ -10,6 +10,9 @@ sub new{ # create new maps object, my $map = lib::GMPAS::new();
         $self->{_created} = 1;
 	$slat = $_[1];
 	$slon = $_[2]; # get these for the map creation
+	$slat = "-".$slat;
+	$slat =~ s/(W|N)$//;
+	$slon =~ s/(W|N)$//;
         return $self;  # Google Maps API allows a map center position.
 }
 sub center_map{
@@ -30,15 +33,28 @@ sub pos_mark{ # create a position object and marker:
 	return $p; # these increment togther p0->m0, p1->m1, etc..
 }
 sub con_str{ # create content string for InfoWindow: (ADD 1 to $_[n])
-	my $c = '\'<div class="infowindow"><b>BSSID:</b>' . $_[1]
-		. '<br /><b>ESSID:</b>' . $_[5]
+=pod
+	my $c = '\'<div class="infowindow">'
+		. '<b>ESSID: </b><span class="essid">' . $_[5] . "</span>"
+		. '<br /><b>BSSID:</b>' . $_[1]
 		. '<br /><b>Channel:</b>' . $_[2]
 		. '<br /><b>Power:</b>' . $_[3]
 		. '<br /><b>Security:</b>' . $_[4]
 		. '<br /><b>Lat:</b>' . $_[6]
 		. '<br /><b>Lon:</b>' . $_[7]
 		. '</div>\';'; # complete
-	return $c;
+=cut
+	my $contents = '\'<div class="infowindow">'.
+		'<table>'.
+		'<tr><td>ESSID</td><td><span class="essid">'.$_[5].'</span></td></tr>'.
+		'<tr><td>BSSID</td><td>'.$_[1].'</td></tr>'.
+		'<tr><td>Channel</td><td>'.$_[2].'</td></tr>'.
+		'<tr><td>Signal</td><td>'.$_[3].'</td></tr>'.
+		'<tr><td>Security</td><td>'.$_[4].'</td></tr>'.
+		'<tr><td>Lat</td><td>'.$_[6].'</td></tr>'.
+		'<tr><td>Lon</td><td>'.$_[7].'</td></tr>'.
+		'</table>\''; # table is new, it's kind of tabular data, amirite?
+	return $contents;
 }
 sub con_str_bt{
 	chomp $_[4];
